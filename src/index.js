119ae552da30d3077d61ba9b1b5c29a77d262cb2
root88 <workintellect@gmail.com>
2018-09-20 18:38:21 +0300

    quadratic-equation: implemented function solveEquation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  equation = equation.replace(/\s|\^2|\+|\*/g, '');
+  let arr = equation.split("x");
+  let a,b,c;
+
+    if(arr[0] == "") {
+      a = 1;
+    }else if(arr[0] == "-") {
+      a = -1;
+    }else{
+      a = +arr[0];
+    }
+
+    if(arr[1] == "") {
+      b = 1;
+    }else if(arr[1] == "-") {
+      b = -1;
+    }else{
+      b = +arr[1];
+    }
+
+    if(arr[2]=="") {
+      c=1;
+    }else if(arr[2]=="-") {
+      c = -1;
+    }else{
+      c = +arr[2];
+    }
+
+
+  let D = b*b - 4*a*c;
+  let rootD = Math.sqrt(D);
+  let x1 = Math.round((-b + rootD)/(2*a));
+  let x2 = Math.round((-b - rootD)/(2*a));
+
+  if(x1>x2){
+    let n = x1;
+    x1 = x2;
+    x2 = n;
+  }
+
+  return [x1, x2];
 }
